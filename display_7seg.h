347474771bb61ca13fa4386f643f248a0cd2cd60
Santiago Hormazabal <santiagohssl@gmail.com>
2020-02-10 14:02:01 -0300

    Add proper display routines for Volts. Add initial support for Amps display.

--- display_7seg.h
+++ display_7seg.h
@@ -3,8 +3,35 @@
 
 #include <stdint.h>
 
+enum
+{
+    SEVEN_SEG_DP_NONE = 0,
+    SEVEN_SEG_DP_RIGHTMOST = 1,
+    SEVEN_SEG_DP_MIDDLE = 2,
+    SEVEN_SEG_DP_LEFTMOST = 4,
+
+    SEVEN_SEG_DP_ALL = 7,
+};
+
+enum
+{
+    SEVEN_SEG_DIGITS_NONE = 0,
+    SEVEN_SEG_DIGITS_RIGHTMOST = 1,
+    SEVEN_SEG_DIGITS_MIDDLE = 2,
+    SEVEN_SEG_DIGITS_LEFTMOST = 4,
+
+    SEVEN_SEG_DIGITS_ALL = 7,
+};
+
+enum
+{
+    SEVEN_SEG_FIRST_ROW = 0,
+    SEVEN_SEG_SECOND_ROW = 3,
+};
+
 void set_display_from_int(uint16_t number, 
                           uint8_t row,
-                          uint8_t decimal_dots);
+                          uint8_t decimal_dots,
+                          uint8_t digits);
 
 #endif /* __7SEGDISPLAY_H */
--- main.c
+++ main.c
@@ -32,11 +32,14 @@ static void do_calibration(void)
 {
     eeprom_settings_t new_settings;
 
+    /* Use the default settings as a starting point */
+    new_settings = default_settings;
+
     /* This should enable the ADC interrupts */
     __enable_interrupt();
 
     /* Wait a moment so the ADC settles */
-    delay(80000);
+    delay(100000);
 
     /**
      * Current ADC values should correspond to an absolute zero,
@@ -53,39 +56,70 @@ static void do_calibration(void)
     __disable_interrupt();
 }
 
+static void set_display_from_double(
+  const double value,
+  const uint8_t pos
+)
+{
+    /* Clamp to zero */
+    if (value < 0.0)
+    {
+        set_display_from_int(
+            0,
+            pos,
+            SEVEN_SEG_DP_LEFTMOST,
+            SEVEN_SEG_DIGITS_ALL
+        );
+    }
+    else if (value >= 100.0)
+    {
+        /* 100. - 999. */
+        set_display_from_int(
+            value,
+            pos,
+            SEVEN_SEG_DP_RIGHTMOST,
+            SEVEN_SEG_DIGITS_ALL
+        );
+    }
+    else if (value >= 10.0)
+    {
+        /* 10.0 - 99.9 */
+        set_display_from_int(
+            value * 10.0,
+            pos,
+            SEVEN_SEG_DP_MIDDLE,
+            SEVEN_SEG_DIGITS_ALL
+        );
+    }
+    else if (value >= 1.0)
+    {
+        /* 1.00 - 9.99 */
+        set_display_from_int(
+            value * 100.0,
+            pos,
+            SEVEN_SEG_DP_LEFTMOST,
+            SEVEN_SEG_DIGITS_ALL
+        );
+    }
+    else
+    {
+        /* .000 - .999 */
+        set_display_from_int(
+            value * 1000.0,
+            pos,
+            SEVEN_SEG_DP_NONE,
+            SEVEN_SEG_DIGITS_ALL
+        );
+    }
+}
+
 static void do_measure(void)
 {
     double value;
     const eeprom_settings_t * settings;
 
+    /* Read the ADC settings */
     settings = eeprom_get_settings();
-
-    /* Raw value from ADC */
-    value = get_adc_amps();
-    /* Offset */
-    value -= settings->adc_amps.offset;
-    /* Scale */
-    value *= settings->adc_amps.scaling;
-    /* Clamp to zero */
-    if (value < 0.0)
-    {
-        set_display_from_int(0, 3, 1);
-    }
-    else if (value >= 100.0)
-    {
-        /* 100. - 999. */
-        set_display_from_int(value, 3, 4);
-    }
-    else if (value >= 10.0)
-    {
-        /* 10.0 - 99.9 */
-        set_display_from_int(value*10, 3, 2);
-    }
-    else if (value >= 1.0)
-    {
-        /* 1.00 - 9.99 */
-        set_display_from_int(value*100, 3, 1);
-    }
 
     /* Raw value from ADC */
     value = get_adc_volts();
@@ -93,26 +127,15 @@ static void do_measure(void)
     value -= settings->adc_volts.offset;
     /* Scale */
     value *= settings->adc_volts.scaling;
-    /* Clamp to zero */
-    if (value < 0.0)
-    {
-        set_display_from_int(0, 0, 1);
-    }
-    else if (value >= 100.0)
-    {
-        /* 100. - 999. */
-        set_display_from_int(value, 0, 4);
-    }
-    else if (value >= 10.0)
-    {
-        /* 10.0 - 99.9 */
-        set_display_from_int(value*10, 0, 2);
-    }
-    else if (value >= 1.0)
-    {
-        /* 1.00 - 9.99 */
-        set_display_from_int(value*100, 0, 1);
-    }
+    set_display_from_double(value, SEVEN_SEG_FIRST_ROW);
+
+    /* Raw value from ADC */
+    value = get_adc_amps();
+    /* Offset */
+    value -= settings->adc_amps.offset;
+    /* Scale */
+    value *= settings->adc_amps.scaling;
+    set_display_from_double(value, SEVEN_SEG_SECOND_ROW);
 }
 
 int main(void)
@@ -122,13 +145,10 @@ int main(void)
     /* Switch to 16MHz */
     setup_clock(CORE_16MHz);
 
-    /* All segments off by default */
+    /* Set the internal state of all segments to off by default */
     set_segments(0);
     /* And no digit selected */
     select_digit(0xFF);
-
-    /* Set the outputs (except the programming input pin) */
-    setup_gpios();
 
     /* Load a default calibration if the EEPROM's content is not valid */
     if (eeprom_load_settings() != 0)
@@ -139,6 +159,7 @@ int main(void)
 
     /* At this stage the ADC is only needed for the calibration */
     setup_adc();
+    setup_timer1(); // this is the trigger needed for ADC sampling
 
     /**
      * Check if the user wants to calibrate the device by checking the
@@ -146,20 +167,23 @@ int main(void)
      */
     programming_pin_control(1);
 
-    if (read_programming_pin())
+    if (!read_programming_pin())
     {
         /* wait until the user stops removes the jumper on the programming pin */
-        while(read_programming_pin());
+      //  while(!read_programming_pin());
 
+        /* execute the calibration routine */
         do_calibration();
         /* Calibration done, continue */
     }
 
+    /* Set the outputs (except the programming input pin) */
+    setup_gpios();
+
     /* Leave programming pin as output (normal operation) */
     programming_pin_control(0);
 
-    /* Timers can be enabled now, so the 7 seg display gets updated */
-    setup_timer1();
+    /* Timer2 can be enabled now, so the 7 seg display gets updated */
     setup_timer2();
 
     __enable_interrupt();
